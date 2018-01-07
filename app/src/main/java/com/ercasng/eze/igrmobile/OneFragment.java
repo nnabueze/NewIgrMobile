de8b4ed0501d150ad861056fd03e203d48b70264
Opara Nnabueze <villavelle101@gmail.com>
2018-01-07 06:29:54 +0100

    added indicator on collection

--- app/src/main/java/com/ercasng/eze/igrmobile/OneFragment.java
+++ app/src/main/java/com/ercasng/eze/igrmobile/OneFragment.java
@@ -1,6 +1,8 @@
 package com.ercasng.eze.igrmobile;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
@@ -16,7 +18,8 @@ import java.text.DecimalFormat;
  */
 
 public class OneFragment extends Fragment {
-    TextView mTextView, currentMonth, yestarday, today;
+    TextView mTextView, currentMonth, yestarday, today, indictator, indicator2;
+    SharedPreferences preferences;
     public OneFragment() {
         // Required empty public constructor
     }
@@ -35,17 +38,85 @@ public class OneFragment extends Fragment {
         currentMonth =(TextView) v.findViewById(R.id.currentMonth);
         yestarday = (TextView) v.findViewById(R.id.yestarday);
         today =(TextView) v.findViewById(R.id.today);
+        indictator = (TextView) v.findViewById(R.id.indicator);
+        indicator2 = (TextView) v.findViewById(R.id.indicator2);
         setTextParam();
         return v;
     }
 
     public void setTextParam(){
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
         mTextView.setText(numberFormat(preferences.getString("lastMonth",null).toString()));
         currentMonth.setText(numberFormat(preferences.getString("currentMonth",null).toString()));
         yestarday.setText(numberFormat(preferences.getString("yestarday",null).toString()));
         today.setText(numberFormat(preferences.getString("today",null).toString()));
 
+        //current month indicator
+        currentIndicator();
+        todayIndicator();
+
+    }
+
+    private void todayIndicator() {
+        double yesterdayAmount = Double.parseDouble(preferences.getString("yestarday",null));
+        double todayAmount = Double.parseDouble(preferences.getString("today",null));
+
+        if (todayAmount > yesterdayAmount){
+            //Increase = New Number - Original Number
+            double increase = todayAmount - yesterdayAmount;
+
+            //% increase = Increase ÷ Original Number × 100
+            double percentageIncrease = increase / yesterdayAmount* 100;
+
+            indicator2.setText(String.format("%.2f", percentageIncrease)+"%");
+            indicator2.setVisibility(View.VISIBLE);
+            indicator2.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_arrow_drop_up_black_18dp, 0, 0, 0);
+            indicator2.setTextColor(Color.parseColor("#2E7D32"));
+        }
+
+        if (yesterdayAmount > todayAmount){
+            //Increase = New Number - Original Number
+            double decrease = todayAmount - yesterdayAmount;
+
+            //% increase = Increase ÷ Original Number × 100
+            double percentageDecrease = decrease / yesterdayAmount * 100;
+
+            indicator2.setText(String.format("%.2f", percentageDecrease)+"%");
+            indicator2.setVisibility(View.VISIBLE);
+            indicator2.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_arrow_drop_down_black_18dp, 0, 0, 0);
+            indicator2.setTextColor(Color.parseColor("#C62828"));
+        }
+    }
+
+    private void currentIndicator() {
+        //checking increase
+        double lastAmount = Double.parseDouble(preferences.getString("lastMonth",null));
+        double currentAmount = Double.parseDouble(preferences.getString("currentMonth",null));
+        if (currentAmount > lastAmount){
+            //Increase = New Number - Original Number
+            double increase = currentAmount - lastAmount;
+
+            //% increase = Increase ÷ Original Number × 100
+            double percentageIncrease = increase / lastAmount* 100;
+
+            indictator.setText(String.format("%.2f", percentageIncrease)+"%");
+            indictator.setVisibility(View.VISIBLE);
+            indictator.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_arrow_drop_up_black_18dp, 0, 0, 0);
+            indictator.setTextColor(Color.parseColor("#2E7D32"));
+        }
+
+        if (lastAmount > currentAmount){
+            //Increase = New Number - Original Number
+            double decrease = currentAmount - lastAmount;
+
+            //% increase = Increase ÷ Original Number × 100
+            double percentageDecrease = decrease / lastAmount * 100;
+
+            indictator.setText(String.format("%.2f", percentageDecrease)+"%");
+            indictator.setVisibility(View.VISIBLE);
+            indictator.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_arrow_drop_down_black_18dp, 0, 0, 0);
+            indictator.setTextColor(Color.parseColor("#C62828"));
+        }
     }
 
     private String numberFormat(String number){
